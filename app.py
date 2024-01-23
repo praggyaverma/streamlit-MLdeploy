6a907266a0b4be784f09fcfb89fb0b8afeea4131
Praggya <119279862+praggyaverma@users.noreply.github.com>
2024-01-23 13:13:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
-import numpy as np
-import pandas as pd
-import streamlit as st 
 import pickle
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+st.title("Classification of Handwritten Digit")
+
+photo = st.file_uploader(label = "Upload image for classification")
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+st.header("Predicted")
+
+st.text('')
+st.text('')
+st.markdown(
+    '`Create by` [praggyaverma](https://linkedin.com/praggyaverma/) | \
+         `Code:` [GitHub](https://github.com/praggya/streamlit-MLdeploy)')
